65d139815a1c56f53026c49553135e7f186ef87f
Lev Sizov <levsizov2015@outlook.com>
2018-03-07 13:55:18 +0300

    Swagger JSONs and default Vue application

--- proto_gen.sh
+++ proto_gen.sh
@@ -4,4 +4,5 @@ prot=$1
 protoc -I $prot -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/options \
 -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
 --grpc-gateway_out=logtostderr=true:$prot/ \
+--swagger_out=logtostderr=true:$prot/ \
 --go_out=plugins=grpc:$prot $prot/*.proto
